40bfc438f24dead47b55a66d0cb67d9d5a7aea67
To Minh Anh <minhhanhs.2205@gmail.com>
2024-11-17 23:12:45 +0700

    git init

---
+++ binhphuong.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+int n;
+scanf("%d", &n);
+int arr[100];
+int i
+for (i = 0; i < n; i++){
+scanf("%d", &arr[i]);
+int a = pow(arr[i], 2);
+printf("%d ", a);
+}
+return 0;
+}
